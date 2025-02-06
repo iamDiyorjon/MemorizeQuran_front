4d76c19664609bc47e3ae5bc40a1c9bb0e51c96c
ruzfardev <farrukhruzmetov2002@gmail.com>
2025-02-06 15:23:21 +0500

    vite config changed

--- src/shared/api/_base.ts
+++ src/shared/api/_base.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://localhost:5050/api",
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import path from "path";
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // Make sure this points correctly
+    },
+  },
   server: {
     host: true,
     port: 5180,
